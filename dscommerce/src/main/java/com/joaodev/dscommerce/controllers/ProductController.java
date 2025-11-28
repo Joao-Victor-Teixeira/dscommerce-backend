047b7aa3a93a0ba1f9c28058b374bca3c1087792
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-28 16:54:30 -0300

    Teste de acesso a banco de dados
    Foi feito de forma "errada" pois nosso controller depende diretamente do repository, mas será refatorado

--- dscommerce/src/main/java/com/joaodev/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/joaodev/dscommerce/controllers/ProductController.java
@@ -1,16 +1,27 @@
 package com.joaodev.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.joaodev.dscommerce.entities.Product;
+import com.joaodev.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public String teste(){
-        return "Ola Mundo!";
+       Optional<Product> result = repository.findById(1L);
+       Product product = result.get();
+       return product.getName();
     }
 
 }
---
+++ dscommerce/src/main/java/com/joaodev/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.joaodev.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaodev.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
