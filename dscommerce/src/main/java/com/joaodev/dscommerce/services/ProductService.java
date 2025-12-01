9c12418862bb1e38c5dc66180b973d33537df78b
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-01 09:25:28 -0300

    Busca paginada de produtos

--- dscommerce/src/main/java/com/joaodev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/joaodev/dscommerce/services/ProductService.java
@@ -1,7 +1,11 @@
 package com.joaodev.dscommerce.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +23,10 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
