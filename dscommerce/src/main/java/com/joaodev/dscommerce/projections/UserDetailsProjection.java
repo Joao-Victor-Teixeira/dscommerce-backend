51ff240d922ef8fa046078f7cc9b91b18d1f0c1b
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-26 11:58:34 -0300

    Implementa UserDatailsService

---
+++ dscommerce/src/main/java/com/joaodev/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.joaodev.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
---
+++ dscommerce/src/main/java/com/joaodev/dscommerce/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package com.joaodev.dscommerce.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.joaodev.dscommerce.projections.UserDetailsProjection;
+
+@Repository
+public interface UserRepository {
+    
+    @Query(nativeQuery = true, value = """
+				SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+				FROM tb_user
+				INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+				WHERE tb_user.email = :email
+			""")
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+}
