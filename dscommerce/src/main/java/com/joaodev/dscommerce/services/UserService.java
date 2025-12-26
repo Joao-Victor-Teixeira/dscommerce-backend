4e73d11d719f368ff1a729d0d7b9c93b4887e22f
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-26 15:48:36 -0300

    ProductMinDTO consulta

--- dscommerce/src/main/java/com/joaodev/dscommerce/services/UserService.java
+++ dscommerce/src/main/java/com/joaodev/dscommerce/services/UserService.java
@@ -1,7 +1,6 @@
 package com.joaodev.dscommerce.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
