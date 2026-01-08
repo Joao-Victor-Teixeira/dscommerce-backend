b6c02d9e4c9a1167e05c5105b574ac96f68c3f8c
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-08 16:46:17 -0300

    CategoryService tests

---
+++ dscommerce/src/test/java/com/joaodev/dscommerce/tests/CategoryFactory.java
@@ -0,0 +1,14 @@
+package com.joaodev.dscommerce.tests;
+
+import com.joaodev.dscommerce.entities.Category;
+
+public class CategoryFactory {
+
+    public static Category createCategory(){
+        return new Category(1L, "Games");
+    }
+
+    public static Category createCategory(Long id, String name){
+        return new Category(id, name);
+    }
+}
