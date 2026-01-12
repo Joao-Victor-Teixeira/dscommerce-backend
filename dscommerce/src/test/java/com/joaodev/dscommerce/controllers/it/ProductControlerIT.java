5174e69018d5ccf766fa4b126f2fddd68f78d44c
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-12 17:43:08 -0300

    ProductController Tests 2

--- dscommerce/src/test/java/com/joaodev/dscommerce/controllers/it/ProductControlerIT.java
+++ dscommerce/src/test/java/com/joaodev/dscommerce/controllers/it/ProductControlerIT.java
@@ -3,6 +3,7 @@ package com.joaodev.dscommerce.controllers.it;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,13 +49,77 @@ public class ProductControlerIT {
         productName = "Macbook Pro"; 
     }
 
+
+    @Test
+    public void updateShouldReturnProductDTOWhenAdminLoggedAndIdExists() throws Exception {
+        String adminToken = tokenUtil.obtainAccessToken(mockMvc, "alex@gmail.com", "123456");
+        ProductDTO dto = ProductFactory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+                .header("Authorization", "Bearer " + adminToken)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(dto.getName()));
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenAdminLoggedAndNonExistingId() throws Exception {
+        String adminToken = tokenUtil.obtainAccessToken(mockMvc, "alex@gmail.com", "123456");
+        ProductDTO dto = ProductFactory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId)
+                .header("Authorization", "Bearer " + adminToken)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void updateShouldReturnUnprocessableEntityWhenAdminLoggedAndInvalidData() throws Exception {
+        String adminToken = tokenUtil.obtainAccessToken(mockMvc, "alex@gmail.com", "123456");
+        ProductDTO dto = ProductFactory.createProductDTO();
+        dto.setName(""); 
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+                .header("Authorization", "Bearer " + adminToken)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void updateShouldReturnForbiddenWhenClientLogged() throws Exception {
+        String clientToken = tokenUtil.obtainAccessToken(mockMvc, "maria@gmail.com", "123456");
+        ProductDTO dto = ProductFactory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+                .header("Authorization", "Bearer " + clientToken)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isForbidden());
+    }
+
     @Test
     public void findByIdShouldReturnOrderWhenSelfClientLogged() throws Exception {
     
-        String mariaToken = tokenUtil.obtainAccessToken(mockMvc, "maria@gmail.com", "123456");
+        String clientToken = tokenUtil.obtainAccessToken(mockMvc, "maria@gmail.com", "123456");
 
         ResultActions result = mockMvc.perform(get("/orders/{id}", 1L)
-            .header("Authorization", "Bearer " + mariaToken)
+            .header("Authorization", "Bearer " + clientToken)
             .accept(MediaType.APPLICATION_JSON));
 
         result.andExpect(status().isOk());
